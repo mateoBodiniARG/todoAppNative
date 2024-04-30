1a33a630314a2ab7d0b068d902515f7eceee7e90
mateoBodiniARG <bodinidev@gmail.com>
2024-04-30 17:15:59 -0300

    feat: Se han creado endpoints

--- server/app.js
+++ server/app.js
@@ -14,3 +14,46 @@ import {
 
 const app = express();
 app.use(express.json());
+
+app.get("/todos/:id", async (req, res) => {
+  const todos = await getTodosByID(req.params.id);
+  res.status(200).send(todos);
+});
+
+app.get("/todos/shared_todos/:id", async (req, res) => {
+  const todo = await getSharedTodoByID(req.params.id);
+  const author = await getUserByID(todo.user_id);
+  const shared_with = await getUserByID(todo.shared_with_id);
+  res.status(200).send({ author, shared_with });
+});
+
+app.get("/users/:id", async (req, res) => {
+  const user = await getUserByID(req.params.id);
+  res.status(200).send(user);
+});
+
+app.put("/todos/:id", async (req, res) => {
+  const { value } = req.body;
+  const todo = await toggleCompleted(req.params.id, value);
+  res.status(200).send(todo);
+});
+
+app.delete("/todos/:id", async (req, res) => {
+  await deleteTodo(req.params.id);
+  res.status(200).send({ message: "Todo deleted" });
+});
+
+app.post("/todos/shared_todos", async (req, res) => {
+  const { todo_id, user_id, email } = req.body;
+  const userToShare = await getUserByEmail(email);
+  if (!userToShare) {
+    res.status(404).send({ message: "User not found" });
+    return;
+  }
+  const sharedTodo = await shareTodo(todo_id, user_id, userToShare.id);
+  res.status(201).send(sharedTodo);
+});
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
